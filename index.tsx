561a42a92a64a92c6a07a35f1d7b1d04b2922b01
coop-okinawa <higashionna-morito@okinawa.coop>
2025-12-24 16:47:12 +0900

    fix app entry import

--- index.tsx
+++ index.tsx
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./src/lib/App";
 
-ReactDOM.createRoot(
-  document.getElementById("root")!
-).render(
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  throw new Error("root element not found");
+}
+
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
