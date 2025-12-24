f298ec375fdf5f8878d9f95d534b13b48943b21c
coop-okinawa <higashionna-morito@okinawa.coop>
2025-12-24 16:41:50 +0900

    fix: move App entry to src/lib/App and update index.tsx

--- index.tsx
+++ index.tsx
@@ -2,13 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./src/lib/App";
 
-const root = document.getElementById("root");
-
-if (!root) {
-  throw new Error("root element not found");
-}
-
-ReactDOM.createRoot(root).render(
+ReactDOM.createRoot(
+  document.getElementById("root")!
+).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
