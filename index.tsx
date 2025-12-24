65aaf1e340587794a18b5d54fe4ae4ee962a64a9
coop-okinawa <higashionna-morito@okinawa.coop>
2025-12-24 16:37:10 +0900

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./src/App";
+import App from "./src/lib/App";
 
-const root = document.getElementById("root")!;
+const root = document.getElementById("root");
+
+if (!root) {
+  throw new Error("root element not found");
+}
+
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <App />
